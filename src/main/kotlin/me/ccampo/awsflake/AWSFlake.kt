7e9512de3ec358409c623719fa4539bfcce029dd
Chris Campo <ccampo133@users.noreply.github.com>
2016-11-14 15:20:28 -0500

    Add configuration properties

--- src/main/kotlin/me/ccampo/awsflake/AWSFlake.kt
+++ src/main/kotlin/me/ccampo/awsflake/AWSFlake.kt
@@ -1,17 +1,49 @@
 package me.ccampo.awsflake
 
 import com.amazonaws.util.EC2MetadataUtils
+import com.natpryce.konfig.*
+import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import spark.Spark.get
+import spark.Spark.port
 
 val log: Logger = LoggerFactory.getLogger("AWSFlake")
 
+class Config {
+    companion object server : PropertyGroup() {
+        val port by intType
+        val region by stringType
+        val ip by stringType
+    }
+}
+
 fun main(args: Array<String>) {
-    val region = AWSRegion.parse(EC2MetadataUtils.getEC2InstanceRegion())
-    val ip = EC2MetadataUtils.getPrivateIpAddress()
+    val config = systemProperties() overriding
+            EnvironmentVariables() overriding
+            ConfigurationProperties.fromResource("defaults.properties")
+
+    // Load some configuration properties
+    port(config[Config.server.port])
+
+    val region: AWSRegion = when {
+        config[Config.server.region].isBlank() -> AWSRegion.parse(EC2MetadataUtils.getEC2InstanceRegion())
+        else -> {
+            log.warn("Overriding node region with value {}", config[Config.server.region])
+            AWSRegion.parse(config[Config.server.region])
+        }
+    }
+
+    val ip: String = when {
+        config[Config.server.ip].isBlank() -> EC2MetadataUtils.getPrivateIpAddress()
+        else -> {
+            log.warn("Overriding node private IP address with value {}", config[Config.server.ip])
+            config[Config.server.ip]
+        }
+    }
+
     val (oct1, oct2) = ip.split(".").takeLast(2).map { Integer.parseInt(it) }
-    log.info("region = {}, ip = {}, octets = {}, {}", region, ip, oct1, oct2)
+    log.info("Region = {}, IP = {}, Octets = {}, {}", region, ip, oct1, oct2)
 
     get("/id") { req, resp ->
         val minLen: Int? = req.queryParams("minLength")?.toInt()
